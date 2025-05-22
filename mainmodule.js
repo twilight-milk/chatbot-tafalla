74e4c77bf11e68c62def4e76fe2b2a8cd4f34b93
Ash Quinn <112402490+twilight-milk@users.noreply.github.com>
2025-05-22 11:54:40 +0800

    Update mainmodule.js

--- mainmodule.js
+++ mainmodule.js
@@ -1,11 +1,11 @@
-import {GoogleGenerativeAI} from "@google/generative-ai";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const API_KEY = "AIzaSyBOUpifEfGA6baP8wzA_T8XY0oFZc-rTw0";
+const API_KEY = "AIzaSyBOUpifEfGA6baP8wzA_T8XY0oFZc-rTw0"; // (Replace if expired)
+
 const genAI = new GoogleGenerativeAI(API_KEY);
 
 const model = genAI.getGenerativeModel({
-    model: "learnlm-1.5-pro-experimental" //learnlm-1.5-pro-experimental //gemini-1.5-flash
-    //systemInstruction: ""
-    });
+  model: "gemini-1.5-pro-latest"
+});
 
-export {model};
+export { model };
